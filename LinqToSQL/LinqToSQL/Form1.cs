14ce58dd30b9b16bf2f840f73150a3ff70950fc1
Willamar Fernandes <wfs20@hotmail.com>
2014-08-06 21:01:40 -0300

    Projetos

---
+++ LinqToSQL/LinqToSQL/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var db = new NorthWindDataContext();
+
+
+            dataGridView1.DataSource = from o in db.Orders
+                                       join od in db.Order_Details on o.OrderID equals od.OrderID
+                                       join p in db.Products on od.ProductID equals p.ProductID
+                                       select new { o.OrderDate, p.ProductName , od.Quantity, od.UnitPrice };
+            //Procedure
+            //dataGridView1.DataSource = db.CustOrdersDetail(10248);
+
+        }
+    }
+}
